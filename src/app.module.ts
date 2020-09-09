8776baf5fca7f2b0e6b1be4a4350b178c34a4ba7
Frederick Johnson <fritzj96@gmail.com>
2020-09-08 19:03:05 -0500

    added: TypeOrmModule to root app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [AuthModule, UserModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      url: process.env.DATABASE_URL,
+      synchronize: true,
+      autoLoadEntities: true,
+    }),
+    AuthModule,
+    UserModule,
+  ],
   controllers: [],
   providers: [],
 })
